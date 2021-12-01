ce01f8c347d127e24e96581e521ca7af8f616eaf
Remy Frenoy <rfrenoy@gmail.com>
2021-12-01 18:23:51 +0100

    day 1

--- advent_of_code2021/day1.py
+++ advent_of_code2021/day1.py
@@ -1,22 +1,23 @@
 from typing import List, Sequence
 
 
-def nb_increased_values(in_list: Sequence[int]) -> int:
+def nb_increased_values(in_list: Sequence[int], sequence_length: int) -> int:
     nb_inc = 0
-    for i, elt in enumerate(in_list):
-        if i-1 >= 0:
-            if elt > in_list[i-1]:
-                print(f'{elt}>{in_list[i-1]}')
+    for i in range(len(in_list)):
+        if i-1 >= 0 and i+sequence_length-1 < len(in_list):
+            last_three_values = in_list[i-1:i+sequence_length-1]
+            current_three_values = in_list[i:i+sequence_length]
+            if sum(last_three_values) < sum(current_three_values):
                 nb_inc += 1
-            else:
-                print(f'{elt}<={in_list[i-1]}')
-                
     return nb_inc
+
 
 def read_input(filepath: str) -> List[int]:
     with open(filepath, 'r') as in_f:
         return [int(x) for x in in_f.readlines()]
 
+
 if __name__ == '__main__':
     inputs = read_input('../inputs/day1.txt')
-    print(nb_increased_values(inputs))
+    print(nb_increased_values(inputs, 1))
+    print(nb_increased_values(inputs, 3))
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,22 +1,35 @@
-from advent_of_code2021.day import nb_increased_values
+from advent_of_code2021.day1 import nb_increased_values
 
 
 def test_nb_increase_should_return_the_number_of_times_values_have_increased():
     # given
     inputs = [1, 2, 3]
+    sequence_length = 1
 
     # when
-    nb_increased = nb_increased_values(inputs)
+    nb_increased = nb_increased_values(inputs, sequence_length)
 
     # then
     assert nb_increased == 2
     
+def test_nb_increase_should_return_the_number_of_times_values_have_increased_taking_sequence_length_into_account():
+    # given
+    inputs = [1, 2, 3]
+    sequence_length = 2
+
+    # when
+    nb_increased = nb_increased_values(inputs, sequence_length)
+
+    # then
+    assert nb_increased == 1
+
 def test_nb_increase_should_return_zero_on_decreasing_list():
     # given
     inputs = [3, 2, 1]
+    sequence_length = 1
 
     # when
-    nb_increased = nb_increased_values(inputs)
+    nb_increased = nb_increased_values(inputs, sequence_length)
 
     # then
     assert nb_increased == 0
